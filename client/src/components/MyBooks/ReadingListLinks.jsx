12b15a2e077a74add105b7acb99c98897c56d7f3
Vaishnavi Kawthankar <vkawthankar@gmail.com>
2021-11-09 15:13:29 +0530

    spacing for links fixed

--- client/src/components/MyBooks/ReadingListLinks.jsx
+++ client/src/components/MyBooks/ReadingListLinks.jsx
@@ -3,7 +3,7 @@ import styles from './mybooks.module.css';
 function ReadingListLinks({ link }) {
     return(
         <a href = "/home" className = {styles.readingLinks}>
-            <img src = {link.source} alt = {link.source}/>
+            <img style = {{opacity: 1}} src = {link.source} alt = {link.source}/>
             <p>{link.text}</p>
             <img className = {styles.arrow} src = "/assets/Vector13.png " alt = "Vector 13"/>
         </a>
