2d0b440d36d8d052d4a5a209ab7889518fd5ba12
Vaishnavi Kawthankar <vkawthankar@gmail.com>
2021-11-09 14:05:53 +0530

    Added horizontal scroll in continue reading section

--- client/src/components/MyBooks/MyBooks.jsx
+++ client/src/components/MyBooks/MyBooks.jsx
@@ -1,8 +1,13 @@
 import styles from './mybooks.module.css';
 import MyBooksIndividual from "./MyBooksIndividual";
+import { useState, useRef } from "react";
 import {Stack} from '@mui/material';
+import Carousel from "react-elastic-carousel";
 
 function MyBooks() {
+    const [activeItemIndex, setActiveItemIndex] = useState(0);
+    const carouselRef = useRef();
+
     const items = [
         {
             imageUrl: "https://images-na.ssl-images-amazon.com/images/I/51k2g+1mSWL._SX329_BO1,204,203,200_.jpg",
@@ -14,18 +19,43 @@ function MyBooks() {
             imageUrl: "https://images-eu.ssl-images-amazon.com/images/I/41ARHyZ3FuL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
             title: "The Vanishing Half",
             author: "Brit Bennett"
+        }, 
+        {
+            imageUrl: "https://images-eu.ssl-images-amazon.com/images/I/41nzI1lhIVL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+            title: "The Promised Land",
+            author: "Barack Obama"
+        },
+
+        {
+            imageUrl: "https://images-eu.ssl-images-amazon.com/images/I/41gVhoPaE5L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+            title: "Think Like a Monk",
+            author: "Jay Shetty"
         }
     ]
     return (
         <div className = {styles.myBooksPage}>
             <h2 className = {styles.continue}>Continue where you left...</h2>
-            <Stack spacing = {2} direction = "row">
+            <Carousel
+                ref={carouselRef}
+                itemsToShow={3}
+                showArrows={false}
+                pagination={false}
+                onChange={(currentItem) => setActiveItemIndex(currentItem.index)}
+            >
                 {items.map(item => {
-                    return (
-                        <MyBooksIndividual data = {item}/>          
-                    )
-                })}
-            </Stack>
+                            return (
+                                <MyBooksIndividual data = {item}/>          
+                            )
+                        })}
+            </Carousel>
+
+            <div className = {styles.booksNavigationLinks}>
+                <p>All Books</p>
+            </div>
+                
+            
+
+
         </div>
     )
 }
--- client/src/components/MyBooks/MyBooksIndividual.jsx
+++ client/src/components/MyBooks/MyBooksIndividual.jsx
@@ -1,8 +1,11 @@
 import styles from './mybooks.module.css';
 
 function MyBooksIndividual({data}) {
+    const handleClick = () => {
+        window.location.pathname = "/home";
+    }
     return (
-        <div>
+        <div onClick = {handleClick} className = {styles.book}>
             <img className = {styles.bookImg} src = {data.imageUrl} alt = {data.title}/>
             <p className = {styles.bookTilte}>{data.title}</p>
             <p className = {styles.bookAuthor}>by {data.author}</p>
