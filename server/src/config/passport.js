2c50ab3a23e56c1812f4c61386cb19031d05e1c4
Prashanth2856 <prashanthnani2856@gmail.com>
2021-11-10 21:07:08 +0530

    facebook authentication implement

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -4,6 +4,7 @@ const {v4: uuidV4} = require('uuid');
 const { newToken } = require("../controllers/auth.controller");
 
 var GoogleStrategy = require('passport-google-oauth2').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 const User = require('../models/user.model');
 const UserBookList = require('../models/userBookList.model');
@@ -59,5 +60,27 @@ passport.use(new GoogleStrategy({
     }
   }
 ));
+
+
+/*--------------FACEBOOK----------*/
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      callbackURL: "http://localhost:5000/auth/facebook/callback",
+      profileFields:['id','displayName','name','gender','email']
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+        console.log(profile);
+        return cb(null,profile);j
+      // });
+    }
+  )
+);
+
+
+
 
 module.exports = passport;
