fdca153de9fed6a93419546f576d690858f40ed8
vcow10kar <vkawthankar@gmail.com>
2021-11-12 20:18:16 +0530

    profile page designed

--- client/src/components/Profile/MainProfilePage.jsx
+++ client/src/components/Profile/MainProfilePage.jsx
@@ -1,39 +1,62 @@
 import styles from "./mainProfilePage.module.css";
 import Switch from '@mui/material/Switch';
+import { useState } from "react";
 
 
 function MainProfilePage() {
     const label = { inputProps: { 'aria-label': 'Switch demo' } };
+    const [mode, setMode] = useState(false);
+
+    const handleChange = (e) => {
+        console.log(e.target.value);
+        setMode(!mode);
+        console.log(mode);
+    }
     return (
-        <div>
+        <div className = {mode ? styles.darkProfilePage : styles.lightProfilePage}>
             <div className = {styles.header}>
                 <img src = "/assets/leftArrow.png" alt = "arrow"/>
                 <p>Edit Profile</p>
             </div>
 
+            <div>
+                <div className = {styles.profilePicture}>
+
+                </div>
+
+                <p className = {styles.userName}>User Name <img src = "/assets/bi_pencil-square.png" alt = "pencil"/></p>
+            </div>
+
 
             <div>
-                <div>
+                <div className = {styles.userNameInput}>
                     <p>Username</p>
 
-                    <input type = "text" />
+                    <div className = {styles.editUserName}>
+                        <input type = "text" defaultValue = "username"  className = {styles.editUser}/>
+                        <img src = "/assets/bi_pencil-square.png" alt = "pencil"/>
+                    </div>
                 </div>
 
-                <div>
+                <div className = {styles.userNameInput}>
                     <p>Email address</p>
 
-                    <input type = "text" />
+                    <div className = {styles.editUserName}>
+                        <input type = "text" defaultValue = "email@domain.com" className = {styles.editUser}/>
+                        <img src = "/assets/bi_pencil-square.png" alt = "pencil"/>
+                    </div>
+                    
                 </div>
 
 
-                <div>
+                <div className = {styles.changePassword}>
                    <p>Change Password</p>
                    <img src = "/assets/Vector13.png" alt = "icon"/>
                 </div>
 
-                <div>
+                <div  className = {styles.nightmode}>
                    <p>Night Mode</p>
-                   <Switch {...label} defaultChecked color="default" />
+                   <Switch {...label} onChange = {handleChange} color="default" />
                 </div>
             </div>
         </div>
