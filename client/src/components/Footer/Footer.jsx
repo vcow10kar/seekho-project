b9cdbe2f321b7d9125771d9945d22c41a36fda73
vcow10kar <vkawthankar@gmail.com>
2021-11-12 10:11:35 +0530

    footer css fixed as a module file

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
 import CollectionsBookmarkOutlinedIcon from "@mui/icons-material/CollectionsBookmarkOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
-import "./footer.css";
+import styles from "./footer.module.css";
 function Footer() {
   const goToHome = () => {
     window.location.pathname = '/home';
@@ -26,26 +26,26 @@ function Footer() {
   }
 
   return (
-    <div className = "footerComponent">
-      <Box className="footer">
-        <div onClick = {goToHome} className="footer_homebtn">
+    <div className = {styles.footerComponent}>
+      <Box className={styles.footer}>
+        <div onClick = {goToHome} className={styles.footer_homebtn}>
           <img src = "/assets/bx_bx-home.png" alt = "Home"/>
           <div>Home</div>
         </div>
-        <div onClick = {goToExplore} className="footer_explorebtn">
+        <div onClick = {goToExplore} className={styles.footer_explorebtn}>
         <img src = "/assets/ic_outline-explore.png" alt = "Explore"/>
           <div>Explore</div>
         </div>
-        <div onClick = {goToMyBooks} className="footer_booksbtn">
+        <div onClick = {goToMyBooks} className= {styles.footer_booksbtn}>
           <img src = "/assets/bx_bx-book.png" alt = "Book"/>
           <div>My Books</div>
         </div>
-        <div onClick = {goToProfile} className="footer_profilebtn">
+        <div onClick = {goToProfile} className={styles.footer_profilebtn}>
           <img src = "/assets/iconoir_profile-circled.png" alt = "Profile"/>
           <div>Profile</div>
         </div>
       </Box>
-      <div className = "footerBottom">
+      <div className = {styles.footerBottom}>
 
       </div>
     </div>
